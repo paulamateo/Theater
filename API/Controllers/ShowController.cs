9aa16b5e4a9ae732780c4ba3a9fe6b68236a9dad
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-03-15 20:56:09 +0100

    Creación de filtros y ordenarlos (en proceso)

--- API/Controllers/ShowController.cs
+++ API/Controllers/ShowController.cs
@@ -28,15 +28,34 @@ public class ShowController : ControllerBase {
         return show;
     }
 
-    [HttpGet("ByName/{title}")]
-    public ActionResult<Show> GetByTitle(string title) {
-        var show = _showService.GetShowByTitle(title);
+    // [HttpGet("ByName/{title}")]
+    // public ActionResult<Show> GetByTitle(string title) {
+    //     var show = _showService.GetShowByTitle(title);
 
+    //     if(show == null)
+    //         return NotFound();
+
+    //     return show;
+    // }
+
+    [HttpGet("search/")]
+    public ActionResult<IEnumerable<Show>> SearchShowByName ([FromQuery] string title) {
+        var show = _showService.GetShowByTitle(title);
         if(show == null)
             return NotFound();
 
-        return show;
+        return Ok(show); 
     }
+
+    [HttpGet("/ordered")]
+    public ActionResult<IEnumerable<Show>> GetAllOrderedByTitle() {
+        var shows = _showService.GetAllShows()
+                                .OrderBy(s => s.Title)
+                                .ToList();
+
+        return shows;
+    }
+
 
     [HttpGet("{showId}/Session")]
     public ActionResult<List<Session>> GetSessionsByShow(int showId) {
